b8acb8726d733d42113aea0249156849b749e441
GedMarc <ged_marc@hotmail.com>
2018-09-11 00:22:01 +0200

    Lint

--- src/test/java/com/jwebmp/guicedpersistence/c3p0/GuicedPersistenceBindingTest.java
+++ src/test/java/com/jwebmp/guicedpersistence/c3p0/GuicedPersistenceBindingTest.java
@@ -1,17 +1,22 @@
 package com.jwebmp.guicedpersistence.c3p0;
 
 import com.jwebmp.guicedinjection.GuiceContext;
+import com.jwebmp.guicedpersistence.db.AsyncPostStartup;
 import com.jwebmp.logger.LogFactory;
 import org.junit.jupiter.api.Test;
 
+import java.util.concurrent.TimeUnit;
 import java.util.logging.Level;
 
 class GuicedPersistenceBindingTest
 {
 	@Test
-	public void testMe()
+	public void testMe() throws InterruptedException
 	{
 		LogFactory.configureConsoleSingleLineOutput(Level.FINE);
 		GuiceContext.inject();
+
+		AsyncPostStartup.getDbAutoStartupExecutors()
+		                .awaitTermination(10, TimeUnit.MINUTES);
 	}
 }
