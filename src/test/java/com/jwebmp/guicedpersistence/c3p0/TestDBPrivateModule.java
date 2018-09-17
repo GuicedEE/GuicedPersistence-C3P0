3c4fe14d978afc3261b5eb918117de7f8161ec9d
GedMarc <ged_marc@hotmail.com>
2018-09-17 12:49:10 +0200

    Smaller fixes and formatter correction

--- src/test/java/com/jwebmp/guicedpersistence/c3p0/TestDBPrivateModule.java
+++ src/test/java/com/jwebmp/guicedpersistence/c3p0/TestDBPrivateModule.java
@@ -5,6 +5,7 @@ import com.jwebmp.guicedpersistence.db.ConnectionBaseInfo;
 import com.jwebmp.guicedpersistence.jpa.JPAConnectionBaseInfo;
 import com.oracle.jaxb21.PersistenceUnit;
 
+import javax.validation.constraints.NotNull;
 import java.lang.annotation.Annotation;
 import java.util.Properties;
 
@@ -12,24 +13,28 @@ public class TestDBPrivateModule
 		extends AbstractDatabaseProviderModule
 {
 
-	@Override
-	protected ConnectionBaseInfo getConnectionBaseInfo(PersistenceUnit unit, Properties filteredProperties)
-	{
-		return new JPAConnectionBaseInfo().setPersistenceUnitName(unit.getName());
-	}
-
-	@Override
-	protected String getJndiMapping()
-	{
-		return "jdbc/jndi";
-	}
-
+	@NotNull
 	@Override
 	protected String getPersistenceUnitName()
 	{
 		return "guiceinjectionh2test";
 	}
 
+	@NotNull
+	@Override
+	protected ConnectionBaseInfo getConnectionBaseInfo(PersistenceUnit unit, Properties filteredProperties)
+	{
+		return new JPAConnectionBaseInfo().setPersistenceUnitName(unit.getName());
+	}
+
+	@NotNull
+	@Override
+	protected String getJndiMapping()
+	{
+		return "jdbc/jndi";
+	}
+
+	@NotNull
 	@Override
 	protected Class<? extends Annotation> getBindingAnnotation()
 	{
