74d1456ccfc55dfa2067816dc39d4d30fd93343a
GedMarc <ged_marc@hotmail.com>
2018-10-01 02:37:49 +0200

    Go JRE 11!!!

--- src/test/java/com/jwebmp/guicedpersistence/c3p0/GuicedPersistenceBindingTest.java
+++ src/test/java/com/jwebmp/guicedpersistence/c3p0/GuicedPersistenceBindingTest.java
@@ -1,11 +1,9 @@
 package com.jwebmp.guicedpersistence.c3p0;
 
 import com.jwebmp.guicedinjection.GuiceContext;
-import com.jwebmp.guicedpersistence.db.AsyncPostStartup;
 import com.jwebmp.logger.LogFactory;
 import org.junit.jupiter.api.Test;
 
-import java.util.concurrent.TimeUnit;
 import java.util.logging.Level;
 
 public class GuicedPersistenceBindingTest
@@ -16,7 +14,5 @@ public class GuicedPersistenceBindingTest
 		LogFactory.configureConsoleSingleLineOutput(Level.FINE);
 		GuiceContext.inject();
 
-		AsyncPostStartup.getExecutionService()
-		                .awaitTermination(10, TimeUnit.MINUTES);
 	}
 }
