d071c7e880011e8ac1f8c1bbcd47345447cae75a
GedMarc <ged_marc@hotmail.com>
2018-10-31 12:54:55 +0200

    Remove Serializable by default, Update module-info to utilize transitive dependencies

--- src/jre10/java/module-info.java
+++ src/jre10/java/module-info.java
@@ -4,8 +4,8 @@ import com.jwebmp.guicedpersistence.services.PropertiesEntityManagerReader;
 module com.jwebmp.guicedpersistence.c3p0 {
 	exports com.jwebmp.guicedpersistence.c3p0.implementations;
 
-	requires com.jwebmp.guicedpersistence;
-	requires com.jwebmp.guicedpersistence.readers.hibernateproperties;
+	requires transitive com.jwebmp.guicedpersistence;
+	requires transitive com.jwebmp.guicedpersistence.readers.hibernateproperties;
 
 	provides PropertiesEntityManagerReader with C3P0ConnectionPropertiesReader;
 }
