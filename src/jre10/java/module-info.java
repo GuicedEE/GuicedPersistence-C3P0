6f6a766cbcb5c275bc395b6e81c4725236204f17
GedMarc <ged_marc@hotmail.com>
2018-11-05 21:39:28 +0200

    Inclusions and Exclusions, base package change to correct for jquery ui themes, bootstrap tags update, smart wizard split for bootstrap 4

--- src/jre10/java/module-info.java
+++ src/jre10/java/module-info.java
@@ -1,4 +1,7 @@
+import com.jwebmp.guicedinjection.interfaces.IGuiceScanJarExclusions;
+import com.jwebmp.guicedinjection.interfaces.IGuiceScanModuleExclusions;
 import com.jwebmp.guicedpersistence.c3p0.implementations.C3P0ConnectionPropertiesReader;
+import com.jwebmp.guicedpersistence.c3p0.implementations.C3P0ModuleExclusions;
 import com.jwebmp.guicedpersistence.services.PropertiesEntityManagerReader;
 
 module com.jwebmp.guicedpersistence.c3p0 {
@@ -6,6 +9,12 @@ module com.jwebmp.guicedpersistence.c3p0 {
 
 	requires com.jwebmp.guicedpersistence;
 	requires com.jwebmp.guicedpersistence.readers.hibernateproperties;
+	requires com.jwebmp.guicedinjection;
+	requires java.validation;
 
 	provides PropertiesEntityManagerReader with C3P0ConnectionPropertiesReader;
+
+	provides IGuiceScanModuleExclusions with C3P0ModuleExclusions;
+	provides IGuiceScanJarExclusions with C3P0ModuleExclusions;
+
 }
---
+++ src/main/java/com/jwebmp/guicedpersistence/c3p0/implementations/C3P0ModuleExclusions.java
@@ -0,0 +1,37 @@
+package com.jwebmp.guicedpersistence.c3p0.implementations;
+
+import com.jwebmp.guicedinjection.interfaces.IGuiceScanJarExclusions;
+import com.jwebmp.guicedinjection.interfaces.IGuiceScanModuleExclusions;
+
+import javax.validation.constraints.NotNull;
+import java.util.HashSet;
+import java.util.Set;
+
+public class C3P0ModuleExclusions
+		implements IGuiceScanJarExclusions<C3P0ModuleExclusions>,
+				           IGuiceScanModuleExclusions<C3P0ModuleExclusions>
+{
+	@Override
+	public @NotNull Set<String> excludeJars()
+	{
+		Set<String> strings = new HashSet<>();
+		strings.add("c3p0-*");
+		strings.add("hibernate-c3p0-*");
+		return strings;
+	}
+
+	@Override
+	public @NotNull Set<String> excludeModules()
+	{
+		Set<String> strings = new HashSet<>();
+
+		strings.add("com.jwebmp.guicedpersistence.c3p0");
+
+		strings.add("com.jwebmp.guicedpersistence");
+		strings.add("com.jwebmp.guicedpersistence.readers.hibernateproperties");
+		strings.add("com.jwebmp.guicedinjection");
+		strings.add("java.validation");
+
+		return strings;
+	}
+}
