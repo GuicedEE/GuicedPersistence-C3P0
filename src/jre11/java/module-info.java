4426d5ae776c41cb7395adbb2beff77c6893b769
GedMarc <ged_marc@hotmial.com>
2020-03-02 10:28:11 +0200

    Module Linting

--- src/jre11/java/module-info.java
+++ src/jre11/java/module-info.java
@@ -15,7 +15,4 @@ module com.guicedee.guicedpersistence.c3p0 {
 
 	provides com.guicedee.guicedpersistence.services.IPropertiesEntityManagerReader with com.guicedee.guicedpersistence.c3p0.implementations.C3P0ConnectionPropertiesReader;
 
-	provides com.guicedee.guicedinjection.interfaces.IGuiceScanModuleExclusions with com.guicedee.guicedpersistence.c3p0.implementations.C3P0ModuleExclusions;
-	provides com.guicedee.guicedinjection.interfaces.IGuiceScanJarExclusions with com.guicedee.guicedpersistence.c3p0.implementations.C3P0ModuleExclusions;
-
 }
