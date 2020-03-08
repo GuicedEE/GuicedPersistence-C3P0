d226766a4e7bb9b05957002737e3aec42d327c98
GedMarc <ged_marc@hotmial.com>
2020-03-08 17:00:21 +0200

    Remove Jar syntax as JDK 8 is no longer supported

--- src/main/java/com/guicedee/guicedpersistence/c3p0/implementations/C3P0ModuleExclusions.java
+++ src/main/java/com/guicedee/guicedpersistence/c3p0/implementations/C3P0ModuleExclusions.java
@@ -1,6 +1,5 @@
 package com.guicedee.guicedpersistence.c3p0.implementations;
 
-import com.guicedee.guicedinjection.interfaces.IGuiceScanJarExclusions;
 import com.guicedee.guicedinjection.interfaces.IGuiceScanModuleExclusions;
 
 import javax.validation.constraints.NotNull;
@@ -8,19 +7,8 @@ import java.util.HashSet;
 import java.util.Set;
 
 public class C3P0ModuleExclusions
-		implements IGuiceScanJarExclusions<C3P0ModuleExclusions>,
-				           IGuiceScanModuleExclusions<C3P0ModuleExclusions>
+		implements  IGuiceScanModuleExclusions<C3P0ModuleExclusions>
 {
-	@Override
-	public @NotNull Set<String> excludeJars()
-	{
-		Set<String> strings = new HashSet<>();
-		strings.add("c3p0-*");
-		strings.add("mchange-commons-*");
-		strings.add("hibernate-c3p0-*");
-		return strings;
-	}
-
 	@Override
 	public @NotNull Set<String> excludeModules()
 	{
