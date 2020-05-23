fbaa3769bcde0f724bcb90b2f079532a14dd0a4b
GedMarc <ged_marc@hotmail.com>
2020-05-23 06:19:36 +0200

    Merging of JPA, BTM, Hibernate Properties and System Properties into GuicedPersistence

--- src/jre11/java/module-info.java
+++ src/jre11/java/module-info.java
@@ -1,17 +1,7 @@
 module com.guicedee.guicedpersistence.c3p0 {
-
 	exports com.guicedee.guicedpersistence.c3p0;
-
-	requires com.guicedee.guicedpersistence;
-	requires com.guicedee.guicedpersistence.readers.hibernateproperties;
-	requires com.guicedee.guicedinjection;
-	requires java.validation;
-	requires java.sql;
-	requires com.guicedee.guicedpersistence.jpa;
+	requires transitive com.guicedee.guicedpersistence;
 	requires c3p0;
-	requires com.google.guice;
-
-	requires java.persistence;
 
 	provides com.guicedee.guicedpersistence.services.IPropertiesEntityManagerReader with com.guicedee.guicedpersistence.c3p0.implementations.C3P0ConnectionPropertiesReader;
 
--- src/moditect/module-info.java
+++ src/moditect/module-info.java
@@ -1,17 +1,6 @@
 module com.guicedee.guicedpersistence.c3p0 {
-
 	exports com.guicedee.guicedpersistence.c3p0;
-
-	requires com.guicedee.guicedpersistence;
-	requires com.guicedee.guicedpersistence.readers.hibernateproperties;
-	requires com.guicedee.guicedinjection;
-	requires java.validation;
-	requires java.sql;
-	requires com.guicedee.guicedpersistence.jpa;
-	requires com.google.guice;
-
-	requires java.persistence;
-
+	requires transitive com.guicedee.guicedpersistence;
 	provides com.guicedee.guicedpersistence.services.IPropertiesEntityManagerReader with com.guicedee.guicedpersistence.c3p0.implementations.C3P0ConnectionPropertiesReader;
 
 }
